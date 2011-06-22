deac75811894ae90c177fe320fb8df8d309d521a
breton <zen@zenpsycho.com>
2011-06-22 01:10:48 +0000

    beautify tween

--- animation/ngv.tween.js
+++ animation/ngv.tween.js
@@ -14,70 +14,81 @@
 // onexit is fired when the tween isn't happening anymore.
 
 
-
 var global = (function() {
     return this;
 })();
 global.ngv = global.ngv || {};
 
 global.ngv.Tween = function Tween(params) {
-   var istweening=false;
-   var opt = {
-       begin:params.begin||{x:0},
-       end:params.end||{x:1},
-       startTime:params.startTime||0,
-       duration:params.duration==null?1000:params.duration,
-       ease:params.ease||function(x){return x},
-       onchange:params.onchange||function(){},
-       onenter:params.onenter||function(){},
-       onexit:params.onexit||function(){}
-       
-   };
-   
-   var tween = {
-       set:function set(time){
-           var i, t=time-opt.startTime;
-           if(t>=0 && t < opt.duration){
-                
-                for(i in opt.begin){
-                    if(opt.begin.hasOwnProperty(i) && i!=="set" && !isNaN(opt.begin[i])){
-                        tween[i] = opt.end[i] ? 
-                            opt.ease(t / opt.duration) * (opt.end[i] - opt.begin[i]) + opt.begin[i] : 
-                            opt.begin[i];
+    var istweening = false;
+    var opt = {
+        begin: params.begin || {
+            x: 0
+        },
+        end: params.end || {
+            x: 1
+        },
+        startTime: params.startTime || 0,
+        duration: params.duration == null ? 1000 : params.duration,
+        ease: params.ease ||
+        function(x) {
+            return x;
+        },
+        onchange: params.onchange ||
+        function() {},
+        onenter: params.onenter ||
+        function() {},
+        onexit: params.onexit ||
+        function() {}
+
+    };
+
+    var tween = {
+        set: function set(time) {
+            var i, t = time - opt.startTime;
+            if (t >= 0 && t < opt.duration) {
+
+                for (i in opt.begin) {
+                    if (opt.begin.hasOwnProperty(i) && i !== "set" && !isNaN(opt.begin[i])) {
+                        if(!isNaN(opt.end[i])){
+                            tween[i]=opt.ease(t / opt.duration) * (opt.end[i] - opt.begin[i]) + opt.begin[i];
+                        } else {
+                            tween[i]=opt.begin[i];
+                        }
                     }
                 }
-               
-                if(!istweening){
+
+                if (!istweening) {
                     opt.onenter(tween);
-                    istweening=true;
+                    istweening = true;
                 }
                 opt.onchange(tween);
-                
-           
-           } else {
-               for(i in opt.begin){
-                  if(opt.begin.hasOwnProperty(i) && i!=="set"){
-                      tween[i]=opt.begin[i];
-                  }
-               }
-                if(istweening){
-                    for(i in opt.end){
-                       if(opt.end.hasOwnProperty(i) && i!=="set"){
-                           tween[i]=opt.end[i];
-                       }
+
+
+            }
+            else {
+                for (i in opt.begin) {
+                    if (opt.begin.hasOwnProperty(i) && i !== "set") {
+                        tween[i] = opt.begin[i];
                     }
-                   opt.onexit(tween);
-                   istweening=false;
-                   
+                }
+                if (istweening) {
+                    for (i in opt.end) {
+                        if (opt.end.hasOwnProperty(i) && i !== "set") {
+                            tween[i] = opt.end[i];
+                        }
+                    }
+                    opt.onexit(tween);
+                    istweening = false;
+
                 }
 
-           }
-           return tween;
-       }
-   };
-   
-   
-   
-   
-}; 
+            }
+            return tween;
+        }
+    };
 
+
+
+
+};
